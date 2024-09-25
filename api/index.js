8319c86e040a9e79a253bc20c557c030dbb5e329
Juan Torres <juantor16@gmail.com>
2024-09-25 09:12:16 -0300

    update

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: '*'  // or specify your frontend URL, e.g., "https://symphony-ai-challenge.vercel.app"
+    origin: '*',  // You can specify your frontend URL if needed, e.g., "https://symphony-ai-challenge.vercel.app"
   }));
 
 const PORT = 5001;
